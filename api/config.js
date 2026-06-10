6875ea478af80ddc45171b2bdd969fe6369c9aea
Bhumi Ranipa <bhumiranipa12gmail.com>
2026-06-10 15:37:06 +0530

    Third commit

--- api/config.js
+++ api/config.js
@@ -1,16 +1,16 @@
 export default function handler(req, res) {
   res.setHeader('Content-Type', 'application/javascript');
+  res.setHeader('Cache-Control', 'no-store');
   res.send(`
-    const firebaseConfig = {
-      apiKey: "${process.env.FIREBASE_API_KEY}",
-      authDomain: "${process.env.FIREBASE_AUTH_DOMAIN}",
-      projectId: "${process.env.FIREBASE_PROJECT_ID}",
-      storageBucket: "${process.env.FIREBASE_STORAGE_BUCKET}",
-      messagingSenderId: "${process.env.FIREBASE_MESSAGING_SENDER_ID}",
-      appId: "${process.env.FIREBASE_APP_ID}"
-    };
-    firebase.initializeApp(firebaseConfig);
-
-    const ADMIN_UID = "${process.env.ADMIN_UID}";
+const firebaseConfig = {
+  apiKey: "${process.env.FIREBASE_API_KEY}",
+  authDomain: "${process.env.FIREBASE_AUTH_DOMAIN}",
+  projectId: "${process.env.FIREBASE_PROJECT_ID}",
+  storageBucket: "${process.env.FIREBASE_STORAGE_BUCKET}",
+  messagingSenderId: "${process.env.FIREBASE_MESSAGING_SENDER_ID}",
+  appId: "${process.env.FIREBASE_APP_ID}"
+};
+firebase.initializeApp(firebaseConfig);
+const ADMIN_UID = "${process.env.ADMIN_UID}";
   `);
 }
